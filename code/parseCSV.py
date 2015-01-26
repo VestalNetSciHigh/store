77e4851844f30ab49159521e3b75b3f5e30483d4
Experimental1212 <cmhurripsh@gmail.com>
2015-01-25 20:05:49 -0500

    CSV to one-hot sparse matrix
    Fully commented output function

--- code/parseCSV.py
+++ code/parseCSV.py
@@ -17,7 +17,19 @@ TARGET = "NetSci-Data-15w04f"
 OUTPUT = "..\\output"
 
 
-def output(content, filename=TARGET, ext=".txt", charlimit=0, print_in_console=False, overwrite=True):
+def output(content, filename=TARGET, ext=".txt", mode="print", charlimit=0, print_in_console=False, overwrite=True):
+    """
+    Write to a file.
+
+    Parameters:          content: string or other output stream
+                        filename: name of the output file
+                             ext: file extension
+                            mode: "print" | "write", denotes which method to use when writing to file.
+                       charlimit: limits the number of characters to write, 0 for unlimited.
+                print_in_console: when True, also print output in the console.
+                       overwrite: when True, allows existing files to be overwritten.
+    """
+
     if not os.path.exists(OUTPUT):
         os.makedirs(os.path.abspath(OUTPUT))
     else:
@@ -31,8 +43,10 @@ def output(content, filename=TARGET, ext=".txt", charlimit=0, print_in_console=F
         content = content[:charlimit]
 
     f = open(OUTPUT + "\\" + filename + ext, 'w')
-    # write(content)
-    print >> f, content
+    if mode == "write":
+        f.write(content)
+    else:
+        print >> f, content
 
     if print_in_console:
         print filename + ext
@@ -57,5 +71,7 @@ output(json.dumps(data), filename=TARGET[12:]+"_dict", ext=".json")
 # dict to "one-hot" format
 vec = DictVectorizer()
 sparse_matrix = vec.fit_transform(data.itervalues()).toarray()
+feature_names = vec.get_feature_names()
 
-output(sparse_matrix, filename=TARGET[12:]+"_matrix", ext=".txt", charlimit=0, print_in_console=True)
+output(sparse_matrix, filename=TARGET[12:]+"_matrix", ext=".txt", print_in_console=True)
+output(feature_names, filename=TARGET[12:]+"_features", ext=".txt")
