c89f0b9d85b2b300b6517f5e165557a60f7e0651
Experimental1212 <cmhurripsh@gmail.com>
2015-01-27 19:39:46 -0500

    Removed Debug statements

--- code/parseCSV.py
+++ code/parseCSV.py
@@ -61,6 +61,15 @@ def output(content, filename=TARGET, ext=".txt", mode="print", charlimit=0, prin
 
 
 def get_category_bins(width, position, unit='', prefix_unit=False):
+    """
+    Description
+
+    Parameters:       width:
+                   position:
+                       unit:
+                prefix_unit:
+    """
+
     if position < 1:
         return ("Less than " if position == 0 else "Greater than ") + (unit+str(width) if prefix_unit else str(width)+" "+unit)
     else:
@@ -68,9 +77,6 @@ def get_category_bins(width, position, unit='', prefix_unit=False):
         max_num = (unit+str(width * (position + 1) - 1) if prefix_unit else str(width * (position + 1) - 1)+" "+unit)
         return min_num + " - " + max_num
         # todo: add 1000 comma separators to result strings
-
-def print_this_thing_quickly(text):
-    print text
 
 
 # begin program
@@ -80,47 +86,21 @@ reader = csv.DictReader(csvfile)
 # CSV to dict
 data = {}
 tuition_key = 'DRVIC2013.Tuition and fees, 2013-14'  # numerical to categorical
-tuition_interval = 2000  # bins for categorical conversion
+tuition_interval = 5000  # bins for categorical conversion
 for row in reader:
     key = row.pop('unitid')
     # key = row.pop('institution name')  # There exist multiple institutions with the same name but different locations.
 
     # Assign a categorical tuition cost
-    print "READ THIS ---------------------------------- READ THIS ------------------------------------------- READ THIS"
     isConverted = False  # True when numerical to categorical conversion is successful
     for pos in range(0, 9):
-        print "pos = " + str(pos)
-        print "Value: " + str(row[tuition_key])
-        print "Value+1: " + str(int(row[tuition_key]) + 1)
-        print "Compare: " + str(tuition_interval * (pos + 1))
-        print "Compare  left: " + str(row[tuition_key])
-        print "Compare right: " + str(tuition_interval * (pos + 1))
-        print "Result:       " + str(int(row[tuition_key]) < tuition_interval * (pos + 1))
 
         if int(row[tuition_key]) < tuition_interval * (pos + 1):
-            print "Apply conversion"
             row[tuition_key] = get_category_bins(tuition_interval, pos, unit='$', prefix_unit=True)
             isConverted = True
             break
-    print "isConverted? " + str(isConverted)
     if not isConverted:
         row[tuition_key] = get_category_bins(tuition_interval * 10, -1, unit='$', prefix_unit=True)  # Final category
-
-# # following code was not working correctly
-#     row[tuition_key] = "Less than $5,000" if row[tuition_key] < 5000 else row[tuition_key]
-#     row[tuition_key] = "$5,000 - $9,999" if row[tuition_key] < 10000 else row[tuition_key]
-#     row[tuition_key] = "$10,000 - $14,999" if row[tuition_key] < 15000 else row[tuition_key]
-#     row[tuition_key] = "$15,000 - $19,999" if row[tuition_key] < 20000 else row[tuition_key]
-#     row[tuition_key] = "$20,000 - $24,999" if row[tuition_key] < 25000 else row[tuition_key]
-#     row[tuition_key] = "$25,000 - $29,999" if row[tuition_key] < 30000 else row[tuition_key]
-#     row[tuition_key] = "$30,000 - $34,999" if row[tuition_key] < 35000 else row[tuition_key]
-#     row[tuition_key] = "$35,000 - $39,999" if row[tuition_key] < 40000 else row[tuition_key]
-#     row[tuition_key] = "$40,000 - $44,999" if row[tuition_key] < 45000 else row[tuition_key]
-#     row[tuition_key] = "$45,000 - $49,999" if row[tuition_key] < 50000 else row[tuition_key]
-
-    print "FINAL CATEGORY: " + row[tuition_key]
-
-    print row[tuition_key]
 
     # duplicate row handling
     if key in data:
@@ -137,4 +117,4 @@ feature_names = vec.get_feature_names()
 
 
 # output(sparse_matrix, filename=TARGET[12:]+"_matrix", ext=".txt", print_in_console=True)
-# output(feature_names, filename=TARGET[12:]+"_features", ext=".txt", print_in_console=True)
+output(feature_names, filename=TARGET[12:]+"_features", ext=".txt", print_in_console=True)
